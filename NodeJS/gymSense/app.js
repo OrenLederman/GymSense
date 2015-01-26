dae5cb9343b4ba6318e59a60974d89f840e3f89d
Oren Lederman <oren.lederman@gmail.com>
2015-01-25 23:18:19 -0500

    Added NodeJS code for showing the last time the equipment in the gym was used

--- NodeJS/gymSense/app.js
+++ NodeJS/gymSense/app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var updateStat = require('./routes/updateStat');
+var lastActivity= require('./routes/lastActivity');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/updateStat', updateStat);
+app.use('/lastActivity', lastActivity);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
